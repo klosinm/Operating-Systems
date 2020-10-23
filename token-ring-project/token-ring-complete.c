eb4d67f06b894813192dcdb7a6601def01a096a6
klosinm <monicaklosin@gmail.com>
2020-10-23 15:20:31 -0400

    completed project! token ring works

--- token-ring-project/token-ring-complete.c
+++ token-ring-project/token-ring-complete.c
@@ -25,6 +25,7 @@ to run:
 #include <unistd.h>
 
 #define MAX 1024
+
 #define READ 0
 #define WRITE 1
 
@@ -33,12 +34,15 @@ typedef int Pipe[2]; //Pipe array
 
 int main(int argc, char *argv[])
 {
+
     int parent_pid = getpid(); //parent ID
+    //int p1[2]; //pipe process 2
+
     char userWord[MAX];          //user word input
     char ProcessLen[MAX];        //how long the ring process is
     char ProcessWordGoesTo[MAX]; //which process is to recieve word
-    char readMessage[MAX];  //reading input from write
-    int processNum = 1; //keep track of the process
+    char readMessage[MAX];       //reading input from write
+    int processNum = 1;          //keep track of the process
 
     /**************************************
     * Get user input
@@ -66,20 +70,24 @@ int main(int argc, char *argv[])
         printf("Please make the location word goes to less than how long the ring is!\n");
         exit(1);
     }
-   
+
+    //copying user message to message going through pipes
+    strcpy(readMessage, userWord);
+
     //indicate the parent ID
     printf("Parent = %d\n", parent_pid);
 
-    // Create pipes array. 
+    // Create pipes array.
     Pipe pipes[numProcess + 1];
 
     // child id
     int pid;
 
     /**************************************
-    * Create children and pipes
+    * Create pipes
     ***************************************/
-    for (int n = 0; n < numProcess + 1; n++)
+
+    for (int n = 0; n <= numProcess - 2; n++)
     {
         /** make pipe **/
         if (pipe(pipes[n]) < 0)
@@ -87,27 +95,17 @@ int main(int argc, char *argv[])
             perror("Error creating pipe\n");
             exit(1);
         }
+    }
 
+    /**************************************
+    * Create children 
+    ***************************************/
+
+    for (int n = 0; n < numProcess - 2; n++)
+    {
         /** make child **/
-        if ((pid = fork()) == 0){
-            /*
-            have to create mutiple pipes and link them to create token ring!
-            so pipe 0 exists to all children
-            */
-
-            //opening and closing and duplicating files? Magic?
-            for (int x = 0; x < numProcess + 1; x++)
-            {
-                // Close write of pipe.
-                close(pipes[x][WRITE]);
-                // Close read of pipe.
-                close(pipes[x][READ]);
-            }
-            // Close the write pipe the last process.
-            close(pipes[n - 1][WRITE]);
-            // Close the read pipe of the current process.
-            close(pipes[n][READ]);
-
+        if ((pid = fork()) == 0)
+        {
             //once child is created, break out of for loop to go to while loop to read/write
             break;
         }
@@ -115,68 +113,66 @@ int main(int argc, char *argv[])
         {
             /**  parent process  **/
 
-            //opening and closing and duplicating files? Magic?
-            for (int i = 1; i < numProcess; i++)
-            {
-                // Close write of pipe.
-                close(pipes[i][WRITE]);
-                // Close read of pipe.
-                close(pipes[i][READ]);
-            }
-
-            // Close read of the first pipe.
-            close(pipes[0][READ]);
-            // Close write of the last pipe.
-            close(pipes[numProcess][WRITE]);
-
-            /*indexing for child value, called it processNum but  
-             it is the childProcess value*/
-            processNum = n + 1;
+            /*indexing for child value, called it processNum but in 
+            actualitiy it is the childProcess value*/
+            processNum = n + 2;
         }
     }
 
-    //loop through pipes with message
     while (1)
     {
         //initial child
         if (processNum == 1)
         {
             printf("---------------------------------------\n");
-            //printf("INITIAL Process %i (%d)-(PPID: %d) \n", processNum, getpid(), getppid());
-            write(pipes[processNum - 1][WRITE], userWord, sizeof(userWord));
-            printf("INITIAL Process %i (%d)-(PPID: %d) write message: %s\n", processNum, getpid(), getppid(), userWord);
+
+            write(pipes[1][WRITE], readMessage, sizeof(readMessage));
+            printf("INITIAL Process %i (%d)-(PPID: %d) write message: %s\n", processNum, getpid(), getppid(), readMessage);
+            read(pipes[0][READ], readMessage, sizeof(readMessage));
+            printf("Process %i (%d)-(PPID:%d) read message: %s \n", processNum, getpid(), getppid(), readMessage);
+
         }
         /*find child that the message word is supposed to be delievered to
         once found, write "done!" in token vs the user input, and have "done!" read
         for the rest of the program. */
         else if (processNum == ProcessDelivery)
         {
+            //process 2, pipe[]
             read(pipes[processNum - 1][READ], readMessage, sizeof(readMessage));
             printf("Process %i (%d)-(PPID:%d) DELIEVERY! read message: %s \n", processNum, getpid(), getppid(), readMessage);
-            write(pipes[processNum - 1][WRITE], "done!", sizeof("done!"));
+            write(pipes[processNum][WRITE], "done!", sizeof("done!"));
             printf("Process %i (%d)-(PPID:%d) write message: %s\n", processNum, getpid(), getppid(), "done!");
-   
+
         }
         //other child processes
-        else if (processNum < (numProcess ))
+        else if (processNum < (numProcess - 1))
         {
             read(pipes[processNum - 1][READ], readMessage, sizeof(readMessage));
             printf("Process %i (%d)-(PPID:%d) read message: %s \n", processNum, getpid(), getppid(), readMessage);
-            write(pipes[processNum - 1][WRITE], readMessage, sizeof(readMessage));
+            write(pipes[processNum][WRITE], readMessage, sizeof(readMessage));
             printf("Process %i (%d)-(PPID:%d) write message: %s\n", processNum, getpid(), getppid(), readMessage);
-            // close(p1[1]);
+
         }
+        //last process
+        else if (processNum == (numProcess - 1))
+        {
+            read(pipes[processNum - 1][READ], readMessage, sizeof(readMessage));
+            printf("Process %i (%d)-(PPID:%d) read message: %s \n", processNum, getpid(), getppid(), readMessage);
+            write(pipes[0][WRITE], readMessage, sizeof(readMessage));
+            printf("Process %i (%d)-(PPID:%d) write message: %s\n", processNum, getpid(), getppid(), readMessage);
+        }
+
         //format output to have space between processes
         printf(" \n");
-
-        //to not get infinite loop
-        break;
+        // for dramatic effect
+        sleep(1);
     }
+
+    
+    //control C to exit program
 
     /*
     End of program! 
-    The program did not end well, it does not work as Monica hoped it would. 
-    (Not for grade, but i'd like to ask you how to make this work, because its a head-scratcher.)
     */
     exit(0);
 }
