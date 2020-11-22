86f8a227d1773f92e400049e11acd85ef85ba331
klosinm <monicaklosin@gmail.com>
2020-11-22 14:32:03 -0500

    got self to work to now add pause play etc

--- deadlock-project/plot_dir.py
+++ deadlock-project/plot_dir.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from matplotlib import animation
+from matplotlib.widgets import Button, Slider
+
 
 from deadlock import Detection
 core = Detection
@@ -11,99 +13,113 @@ color_map = []
 edge_color_map = []
 shape_map = []
 extraInfo = 0
+
  
-def update(num, G, ax, layout):
-    global extraInfo
-    extraInfo = ""
-    #clear evert loop to change colors
-    edge_color_map = []
-    ax.clear()
-    
-    currentStep = core.input_array[num].split(" ")
-    requestType = currentStep[0]  # "r" or "f"
-    currentProcess = (currentStep[1])  # PID
-    currentResource = (currentStep[2])  # RID
-    print("---")
-    print(" Step" + str(num+1) + ": P" + currentProcess + " " + str(core.verbalrequests[num]) + "'s R" + currentResource)
+class Animation:
+    def update(self, num, G, ax, layout):
+        global extraInfo
+        self.extraInfo = ""
+        #clear evert loop to change colors
+        edge_color_map = []
+        ax.clear()
 
-    point = core.steps[num][0] #element pointing
-    point2 = core.steps[num][1] #element being pointed to
+        self.currentStep = core.input_array[num].split(" ")
+        self.requestType = self.currentStep[0]  # "r" or "f"
+        self.currentProcess = self.currentStep[1]  # PID
+        self.currentResource = self.currentStep[2]  # RID
+       
 
-    #request resource
-    if(requestType == "r" and core.verbalrequests[num] == "owns"):
-        G.add_edge(point, point2)
-    elif(requestType == "r" and core.verbalrequests[num] == "requests"):
-        G.add_edge(point, point2)
-        
-    #freeing resource
-    if (requestType == "f"):
-        G.remove_edge(point, point2)
-        #check if resource is wanted by another process
-        if(num  < (len(core.input_array)-1)):
-            if (core.verbalrequests[num + 1] == "now owns"):
-                #remove P -> R
-                G.remove_edge(core.steps[num + 1][1], core.steps[num + 1][0])
-                #add R -> P
-                G.add_edge(core.steps[num + 1][0], core.steps[num + 1][1])
+        print("Deadlock: ")
+        print(core.deadlockSteps[num])
+        self.point = core.steps[num][0]  # element pointing
+        self.point2 = core.steps[num][1]  # element being pointed to
 
-                extraInfo = "\n " + core.steps[num + 1][1] + " now owns " + core.steps[num + 1][0]
-                #remove next step in step array, since we just did the "now owns" in this current loop
-                del core.verbalrequests[num+1: num + 2]
-                del core.steps[num + 1 : num + 2]
+        #request resource
+        if(self.requestType == "r" and core.verbalrequests[num] == "owns"):
+            G.add_edge(self.point, self.point2)
+        elif(self.requestType == "r" and core.verbalrequests[num] == "requests"):
+            G.add_edge(self.point, self.point2)
+            
+        #freeing resource
+        if (self.requestType == "f"):
+            G.remove_edge(self.point, self.point2)
+            #check if resource is wanted by another process
+            if(num  < (len(core.input_array)-1)):
+                if (core.verbalrequests[num + 1] == "now owns"):
+                    #remove P -> R
+                    G.remove_edge(core.steps[num + 1][1], core.steps[num + 1][0])
+                    #add R -> P
+                    G.add_edge(core.steps[num + 1][0], core.steps[num + 1][1])
 
-   
-    #add edge colors
-    for e in G.edges:
-        print("in egdes loop")
-        if (e[0][0] == "R"):
-            edge_color_map.append("blue")
-        else:
-            edge_color_map.append("red")
-
-    #title
-    ax.set_title("Deadlock Detection \n Step " + str(num + 1) + "/" + str(len(core.input_array)) +
-                 ": P" + str(currentProcess) + " " + core.verbalrequests[num] + "'s R" + str(
-                     currentResource) + extraInfo,
-                 fontweight="bold")
-
-    #Draw graph
-    nx.draw(G,
-            with_labels=True,
-            pos=layout,
-            node_color=color_map,
-            edge_color=edge_color_map,
-            node_shape="s",
-            ax=ax)
-   
-#initial step, just so step 0 doesn't print twice
-def init():
-    pass
+                    extraInfo = "\n " + core.steps[num + 1][1] + " now owns " + core.steps[num + 1][0]
+                    #remove next step in step array, since we just did the "now owns" in this current loop
+                    del core.verbalrequests[num+1: num + 2]
+                    del core.steps[num + 1 : num + 2]
 
 
-def simple_animation():
-    # Build plot
-    fig, ax = plt.subplots(figsize=(6, 5))
-    G = nx.DiGraph()
+        #add edge colors
+        for e in self.G.edges:
+            print(e)
+            if (e[0][0] == "R"):
+                edge_color_map.append("lightsalmon")
+            else:
+                edge_color_map.append("paleturquoise")
 
-    #add process nodes
-    for i in range(core.numProcesses):
-        G.add_node("P" + str(i))
-        color_map.append("paleturquoise")
-        shape_map.append("o")
-
-    #add resource nodes
-    for i in range(core.numResources):
-        G.add_node("R" + str(i))
-        color_map.append("lightsalmon")
-        shape_map.append("v")
-
-    #set graph
-    layout = nx.circular_layout(G)
-
-    ani = animation.FuncAnimation(
-        fig, update, frames=len(core.input_array), init_func=init(), interval=2000, fargs=(G, ax, layout), repeat=False)
-
-    plt.show()
+        #Detect Deadlock, color edges red and stop simulation
+        if (core.deadlockSteps[num] == 1):
+                #print(list(nx.simple_cycles(G)))
+                T = list(nx.simple_cycles(G))
+                print(T[0])           
+                self.extraInfo = "\n Deadlock!"
+                for e in G.edges:
+                    edge_color_map.append("red")
+                #plt.pause(10)
 
 
-simple_animation()
+        #title
+        ax.set_title("Deadlock Detection \n Step " + str(num + 1) + "/" + str(len(core.input_array)) +
+                     ": P" + str(self.currentProcess) + " " + core.verbalrequests[num] + "'s R" + str(
+            self.currentResource) + self.extraInfo,
+                        fontweight="bold")
+
+        #Draw graph
+        nx.draw(G,
+                with_labels=True,
+                pos=layout,
+                node_color=color_map,
+                edge_color=edge_color_map,
+                node_shape="s",
+                ax=ax)
+
+    #initial step, just so step 0 doesn't print twice
+    def init(self):
+        pass
+
+
+    def simple_animation(self):
+        # Build plot
+        self.fig, self.ax = plt.subplots(figsize=(8, 6))
+        self.G = nx.DiGraph()
+
+        #add process nodes
+        for i in range(core.numProcesses):
+            self.G.add_node("P" + str(i))
+            color_map.append("paleturquoise")
+
+        #add resource nodes
+        for i in range(core.numResources):
+            self.G.add_node("R" + str(i))
+            color_map.append("lightsalmon")
+            
+        #set graph
+        self.layout = nx.circular_layout(self.G)
+
+
+
+        ani = animation.FuncAnimation(
+            self.fig, self.update, frames=len(core.input_array), init_func=self.init, interval=2500, fargs=(self.G, self.ax, self.layout), repeat=False)
+
+        plt.show()
+
+
+Animation().simple_animation()
